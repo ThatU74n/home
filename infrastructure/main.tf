e1b6e02cbaff052925f002b25b70883cd6bcff99
utan <admin@admin.com>
2026-04-02 14:24:01 +0700

    feat: add gitea (#1)
    Co-authored-by: utan <utan.dev2358@gmail.com>
    Reviewed-on: http://localhost:3000/utan/home/pulls/1

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,6 +8,25 @@ data "proxmox_virtual_environment_vm" "debian_13_template" {
   vm_id     = 100
 }
 
+module "gitea_vm" {
+  source = "./modules/proxmox_vm"
+
+  proxmox_vm_template_id = data.proxmox_virtual_environment_vm.debian_13_template.vm_id
+
+  proxmox_vm_name             = "gitea-server"
+  proxmox_vm_id               = 200
+  proxmox_vm_cpu_cores        = 2
+  proxmox_vm_memory_dedicated = 4096
+  proxmox_vm_disk_size        = 25
+
+  cloud_init_ipv4_address             = "192.168.1.20/24"
+  cloud_init_ipv4_gateway             = "192.168.1.1"
+  cloud_init_user_account_username    = "gitea"
+  cloud_init_user_account_public_keys = [local.local_public_key]
+
+  tags = ["gitea"]
+}
+
 
 module "control_plane" {
   source = "./modules/proxmox_vm"
